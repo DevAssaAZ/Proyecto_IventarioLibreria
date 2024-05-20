9b561a52e467623c7f864424dde2653f317c4bd5
Peñafiel Del Rosario Gabriel <95557246+DevAssaAZ@users.noreply.github.com>
2024-05-19 21:30:32 -0500

    Cambio en conexion (Capa Datos)

--- Datos/ConexionSQL/ConnectionToSql.cs
+++ Datos/ConexionSQL/ConnectionToSql.cs
@@ -13,7 +13,8 @@ namespace Datos.ConexionSQL
         private readonly string conexionString;
         public ConnectionToSql()
         {
-            conexionString = "server=DESKTOP-H040T0U; database=PROYECTO_INVENTARIO; user=SA; password=evita;";
+            conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
+            //conexionString = "server=DESKTOP-H040T0U; database=PROYECTO_INVENTARIO; user=SA; password=evita;";
 
         }
         protected SqlConnection GetConnection()
