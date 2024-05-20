dd959a8c15ba6831c1d8a575061fbd793f43f122
AnthonyReyes <anthony2003reyes@hotmail.com>
2024-05-20 14:43:40 -0500

    envio de sql

--- Datos/ConexionSQL/ConnectionToSql.cs
+++ Datos/ConexionSQL/ConnectionToSql.cs
@@ -15,6 +15,7 @@ namespace Datos.ConexionSQL
         {
             //conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
             conexionString = "server=DESKTOP-H040T0U; database=PROYECTO_INVENTARIO; user=SA; password=evita;";
+            //conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
 
         }
         protected SqlConnection GetConnection()
