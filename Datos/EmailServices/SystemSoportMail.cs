468575fd1119f78c477a58c0ae4377ababe88dc2
Peñafiel Del Rosario Gabriel <95557246+DevAssaAZ@users.noreply.github.com>
2024-05-21 08:51:01 -0500

    Correo de soporte propio de la libreria

--- Datos/EmailServices/SystemSoportMail.cs
+++ Datos/EmailServices/SystemSoportMail.cs
@@ -11,11 +11,15 @@ namespace Datos.EmailServices
         //Constructor
         public SystemSoportMail()
         {
-            senderMail = "soporteproyectoinventarioug@hotmail.com";
-            password = "saqfapiskssctslo";
+            //senderMail = "soporteproyectoinventarioug@hotmail.com";
+            //password = "saqfapiskssctslo";
+            senderMail = "soporte.proyectoinventarioug@libreriaug.tech";
+            password = "dwOsULw8";
             //senderMail = Environment.GetEnvironmentVariable("SUPPORT_EMAIL") ?? "soporteproyectoinventarioug@hotmail.com";
             //password = Environment.GetEnvironmentVariable("SUPPORT_EMAIL_PASSWORD") ?? "saqfapiskssctslo";
-            host = "smtp-mail.outlook.com";
+            //host = "smtp-mail.outlook.com";
+            //port = 587;
+            host = "us2.smtp.mailhostbox.com";
             port = 587;
             STARTTLS = true;
             inicializarSmtpClient();
