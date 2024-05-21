7594880c52c5ecc1eaf750a757621ace58aa4d66
AnthonyReyes <anthony2003reyes@hotmail.com>
2024-05-20 22:44:32 -0500

    Pantalla de CREAR CUENTA solo eso por el momento

---
+++ Presentacion/Login/CrearCuenta.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Presentacion.Login
+{
+    partial class CrearCuenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CrearCuenta));
+            this.cmbRol = new System.Windows.Forms.ComboBox();
+            this.txtUsuarioRecuperar = new System.Windows.Forms.TextBox();
+            this.txtContraseñaRecuperar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkRecuperarPass = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbRol
+            // 
+            this.cmbRol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.cmbRol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbRol.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.cmbRol.ForeColor = System.Drawing.Color.White;
+            this.cmbRol.FormattingEnabled = true;
+            this.cmbRol.Items.AddRange(new object[] {
+            "Administrador",
+            "Empleado"});
+            this.cmbRol.Location = new System.Drawing.Point(288, 67);
+            this.cmbRol.Name = "cmbRol";
+            this.cmbRol.Size = new System.Drawing.Size(442, 29);
+            this.cmbRol.TabIndex = 0;
+            this.cmbRol.Text = "ROL";
+            // 
+            // txtUsuarioRecuperar
+            // 
+            this.txtUsuarioRecuperar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtUsuarioRecuperar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuarioRecuperar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioRecuperar.ForeColor = System.Drawing.Color.White;
+            this.txtUsuarioRecuperar.Location = new System.Drawing.Point(288, 136);
+            this.txtUsuarioRecuperar.Name = "txtUsuarioRecuperar";
+            this.txtUsuarioRecuperar.Size = new System.Drawing.Size(442, 20);
+            this.txtUsuarioRecuperar.TabIndex = 3;
+            this.txtUsuarioRecuperar.Text = "USUARIO";
+            // 
+            // txtContraseñaRecuperar
+            // 
+            this.txtContraseñaRecuperar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtContraseñaRecuperar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseñaRecuperar.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.txtContraseñaRecuperar.ForeColor = System.Drawing.Color.White;
+            this.txtContraseñaRecuperar.Location = new System.Drawing.Point(288, 199);
+            this.txtContraseñaRecuperar.Name = "txtContraseñaRecuperar";
+            this.txtContraseñaRecuperar.Size = new System.Drawing.Size(442, 20);
+            this.txtContraseñaRecuperar.TabIndex = 3;
+            this.txtContraseñaRecuperar.Text = "CONTRASEÑA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(285, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "_________________________________________________________________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(285, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(445, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "_________________________________________________________________________";
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.ForeColor = System.Drawing.Color.White;
+            this.titulo.Location = new System.Drawing.Point(23, 21);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(212, 33);
+            this.titulo.TabIndex = 7;
+            this.titulo.Text = "CREAR CUENTA";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-3, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(817, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "_________________________________________________________________________________" +
+    "______________________________________________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-3, -2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(817, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "_________________________________________________________________________________" +
+    "______________________________________________________";
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnAcceder.FlatAppearance.BorderSize = 0;
+            this.btnAcceder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcceder.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.ForeColor = System.Drawing.Color.LightGray;
+            this.btnAcceder.Location = new System.Drawing.Point(288, 251);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(199, 44);
+            this.btnAcceder.TabIndex = 12;
+            this.btnAcceder.Text = "REGISTRARSE";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(531, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 44);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "NUEVO";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // linkRecuperarPass
+            // 
+            this.linkRecuperarPass.AutoSize = true;
+            this.linkRecuperarPass.ForeColor = System.Drawing.Color.White;
+            this.linkRecuperarPass.LinkColor = System.Drawing.Color.DimGray;
+            this.linkRecuperarPass.Location = new System.Drawing.Point(605, 314);
+            this.linkRecuperarPass.Name = "linkRecuperarPass";
+            this.linkRecuperarPass.Size = new System.Drawing.Size(50, 13);
+            this.linkRecuperarPass.TabIndex = 14;
+            this.linkRecuperarPass.TabStop = true;
+            this.linkRecuperarPass.Text = "VOLVER";
+            // 
+            // CrearCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(798, 355);
+            this.Controls.Add(this.linkRecuperarPass);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContraseñaRecuperar);
+            this.Controls.Add(this.txtUsuarioRecuperar);
+            this.Controls.Add(this.cmbRol);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CrearCuenta";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CrearCuenta";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbRol;
+        private System.Windows.Forms.TextBox txtUsuarioRecuperar;
+        private System.Windows.Forms.TextBox txtContraseñaRecuperar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkRecuperarPass;
+    }
+}
