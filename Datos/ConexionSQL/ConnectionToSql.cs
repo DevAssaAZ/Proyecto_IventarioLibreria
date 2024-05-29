d65666d1c18bd5ab56c38ce1dd0a2122a99af72f
danielfrancoemp <afrancoemperador@gmail.com>
2024-05-29 16:32:32 -0500

    Cambios en conexión SQL

--- Datos/ConexionSQL/ConnectionToSql.cs
+++ Datos/ConexionSQL/ConnectionToSql.cs
@@ -17,7 +17,8 @@ namespace Datos.ConexionSQL
         {
             //conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
             //conexionString = "server=DESKTOP-H040T0U; database=PROYECTO_INVENTARIO; user=SA; password=evita;";
-            conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
+            //conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
+            conexionString = "server=DESKTOP-FU7K7HH\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=clave123;";
 
         }
         protected SqlConnection GetConnection()
