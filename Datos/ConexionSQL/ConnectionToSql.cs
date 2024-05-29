2472d1c2f7f622343d17c33589b80f9af4b2f276
BRITANY RAFAELA DUQUE BORBOR <britany.duquebor@ug.edu.ec>
2024-05-29 17:24:38 -0500

    cambio

--- Datos/ConexionSQL/ConnectionToSql.cs
+++ Datos/ConexionSQL/ConnectionToSql.cs
@@ -19,6 +19,7 @@ namespace Datos.ConexionSQL
             //conexionString = "server=DESKTOP-H040T0U; database=PROYECTO_INVENTARIO; user=SA; password=evita;";
             //conexionString = "server=DESKTOP-4J9EM3K\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=12345678;";
             conexionString = "server=LAPTOP-49A9F0A5\\SQLEXPRESS; database=PROYECTO_INVENTARIO; user=SA; password=onepiece123;";
+            //
         }
         protected SqlConnection GetConnection()
         {
